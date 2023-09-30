d9f959169e3887a4db8f72f2e2a2da704a97722d
Saki Chy <sakibchowdhury79@gmail.com>
2023-09-30 21:07:23 +0000

    populate the chopped up images in order from 1 to 9 in the game box

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,17 @@ let currentTile;
 let otherTile;
 
 let turns = 0;
+let imgOrder = ["1","2", "3", "4", "5", "6", "7", "8","9"];
 
+window.onload = function() {
+    for (let r=0; r < rows; r++ ) {
+        for (let c=0; c < columns; c++) {
+            let  tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = "assets/images/" + imgOrder.shift() + ".jpg";
+
+            document.getElementById("game-box").append(tile);
+        }
+    }
+}
 
