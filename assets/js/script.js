cf4c26b22846e4c9eb32b5a559ad6f1cea00d022
Saki Chy <sakibchowdhury79@gmail.com>
2023-10-02 19:51:33 +0000

    adjust the code fo rplayingthr puzzle and create a turn function to count the turns of each move

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,92 @@
-// Puzzle Box
-const rows = 3;
-const columns = 3;
 
-let currentTile;
-let otherTile;
+var rows = 3;
+var columns = 3;
 
-let turns = 0;
-let imgOrder = ["5","3", "9", "6", "4", "2", "1", "7","6"];
+var currTile;
+var otherTile; //blank tile
+
+var turns = 0;
+
+var imgOrder = ["1", "2", "7", "4", "8", "6", "9", "3", "5"];
 
 window.onload = function() {
-    for (let r=0; r < rows; r++ ) {
+    for (let r=0; r < rows; r++) {
         for (let c=0; c < columns; c++) {
-            let  tile = document.createElement("img");
+
+            //<img id="0-0" src="1.jpg">
+            let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
             tile.src = "assets/images/" + imgOrder.shift() + ".jpg";
 
+
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart);  //click an image to drag
+            tile.addEventListener("dragover", dragOver);    //moving image around while clicked
+            tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
+            tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
+            tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
+            tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
+
             document.getElementById("game-box").append(tile);
+
         }
     }
 }
 
-/*Drag and Drop function inside puzzle*/
-tile.addEventListener("dragstart", dragStart);
-tile.addEventListener("dragover", dragOver);
-tile.addEventListener("dragenter", dragEnter);
-tile.addEventListener("dragdrop", dragDrop);
-tile.addEventListener("dragleave", dragLeave);
+function dragStart() {
+    currTile = this; //this refers to the img tile being dragged
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; //this refers to the img tile being dropped on
+}
+
+function dragEnd() {
+    if (!otherTile.src.includes("3.jpg")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = r == r2 && c2 == c-1;
+    let moveRight = r == r2 && c2 == c+1;
+
+    let moveUp = c == c2 && r2 == r-1;
+    let moveDown = c == c2 && r2 == r+1;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+
+        turns += 1;
+        document.getElementById("turn-count").innerText = turns;
+    }
+
+
+}
 
 // Background Music
 document.getElementById('play-music').addEventListener('click', function() {
@@ -44,5 +105,9 @@ document.getElementById('play-music').addEventListener('click', function() {
         audio.pause();  // Pause the audio
     }
 });
+
+// Timer Countdown
+
+let timer = 5
 
 
